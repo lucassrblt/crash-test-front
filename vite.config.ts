693de700f3d3047bdae490909037d41fb2940f12
lucassrblt <lrimbault92@gmail.com>
2025-03-16 16:20:33 +0100

    adding workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Crash test pwa",
+        short_name: "pwa",
+        description: "My awesome crash test pwa built with Vite and React!",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./src/assets/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // Enables PWA in development mode
+      },
+    }),
+  ],
+});
