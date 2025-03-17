1948ae2e835e85a88f1f4e98752303280cfadc65
lucassrblt <lrimbault92@gmail.com>
2025-03-17 12:45:56 +0100

    test workflow on prod

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <p>Test Workflow</p>
+        <p>Test Workflow on prod</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
